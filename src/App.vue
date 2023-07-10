0cc8fd27164387fb9be42adbc6cbb1150d5aa11c
danielmegale <danim1@live.it>
2023-07-10 11:54:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppMain from './components/AppMain.vue';
+import AppHeader from './components/AppMain.vue';
+import AppFooter from './components/AppMain.vue';
+export default {
+  components: { AppHeader, AppMain, AppFooter }
+}
+</script>
+
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style></style>
