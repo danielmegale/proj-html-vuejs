d5164201986f86e4c2b9640a8d4ce526a6ef8c06
danielmegale <danim1@live.it>
2023-07-11 17:56:50 +0200

    added gallery hover

--- src/components/MainGallery.vue
+++ src/components/MainGallery.vue
@@ -43,22 +43,75 @@ export default {
 
 
 <template>
-    <section>
-        <div class="d-flex flex-wrap justify-content-between">
-            <div class="my-w-25" v-for="image in images">
+    <section class="container mt-5 pt-5">
+        <div class="d-flex align-items-end mb-5">
+            <div class="w-50">
+                <div class="my-w-80 center">
+                    <div class="small-separator"></div>
+                    <div class="separator"></div>
+                </div>
+                <div>
+                    <h2 class="font-50">Our recent <span class="text-success">web designs</span> & some examples of past
+                        <span class="text-success">projects</span>
+                    </h2>
+                </div>
+                <div class="my-w-80 center">
+                    <div class="small-separator"></div>
+                    <div class="separator"></div>
+                </div>
+            </div>
+            <div class="w-50">
+                <p>Far far away,behind the word mountains,far from the countries Vokalia and Consonatia,there live the
+                    blinds texts. Separated they live in Bookmarkgrove right at the coast of the semantics, a large
+                    language
+                    ocean.Separated they live in Bookmarkgrove.</p>
+            </div>
+        </div>
+        <div class="d-flex flex-wrap justify-content-between mt-5">
+            <div class="my-w-25 gallery-hover" v-for="image in images">
                 <img :src="getImg(image.img)" alt="">
             </div>
+        </div>
+        <div class="d-flex justify-content-center mt-5 pt-5 mb-5">
+            <button class="my-btn box-shadow-green">READ MORE</button>
         </div>
     </section>
 </template>
 
 
 <style lang="scss" scoped>
+@import '../assets/sass/custom';
+
 .my-w-25 {
     padding: 3px 0;
 
     img {
-        border-radius: 15px;
+        border-radius: 5px;
     }
+}
+
+.gallery-hover {
+    position: relative;
+}
+
+.gallery-hover::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-image: linear-gradient(to top right, $color-gradient-green);
+    opacity: 0.9;
+    display: none;
+    border-radius: 5px;
+}
+
+.gallery-hover:hover.gallery-hover::after,
+.gallery-hover:hover img {
+    display: block;
+    transform: scale(1.1);
+    z-index: 1;
 }
 </style>
